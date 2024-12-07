21986f931c87bd9322f4a50340c1d3a1e56a0707
RyuseiNomi <kanontonto1010@gmail.com>
2024-12-08 00:43:12 +0900

    fetch row data from spreadsheet

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const { Client, GatewayIntentBits } = require('discord.js');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { JWT } = require('google-auth-library')
 
 const client = new Client(
   { intents: [
@@ -22,3 +24,22 @@ client.on('ready', () => {
   console.log('ボットが起動したよ');
 });
 client.login(process.env.TOKEN);
+
+loadMembersFromSheet();
+
+async function loadMembersFromSheet() {
+  const serviceAccountAuth = new JWT({
+    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    key: process.env.GOOGLE_PRIVATE_KEY,
+    scopes: [
+      'https://www.googleapis.com/auth/spreadsheets',
+    ]
+  });
+
+  const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID, serviceAccountAuth);
+  await doc.loadInfo();
+  const sheet = await doc.sheetsById[process.env.DEV_WORKSHEET_ID];
+  const rows = await sheet.getRows();
+
+  await console.log(rows)
+}
