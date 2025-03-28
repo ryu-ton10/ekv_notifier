43d20f92cc9abb1f98affdcdadea80b594fb41d1
RyuseiNomi <kanontonto1010@gmail.com>
2025-03-28 15:24:37 +0900

    change shift fetch process from js to ts

--- commands/utility/shift.ts
+++ commands/utility/shift.ts
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js')
-const { getRows, loadShiftFromSheet } = require('./../../workers/shift_fetcher')
+import { SlashCommandBuilder } from 'discord.js'
+import { getRows, loadShiftFromSheet } from './../../workers/shift_fetcher'
+import type { CommandInteraction, CommandInteractionOptionResolver } from "discord.js";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,15 +12,15 @@ module.exports = {
     .addStringOption(option =>
       option.setName('month')
         .setDescription('月')),
-  async execute(interaction) {
-    const year = await interaction.options.getString('year')
-    const month = await interaction.options.getString('month')
+  async execute(interaction: CommandInteraction, options: CommandInteractionOptionResolver) {
+    const year = await options.getString('year')
+    const month = await options.getString('month')
     await getRows().then(rows => {
       if (!year || !month) {
         interaction.reply(`${interaction.user}\n年月を指定した上で、再度コマンドを実行してください。`)
         return
       }
-      message = loadShiftFromSheet(rows, interaction.user.id, year, month)
+      const message = loadShiftFromSheet(rows, interaction.user.id, year, month)
       interaction.reply(`${interaction.user} さんの ${year}年${month}月の参加予定日は以下です。\n${message}\n※突発的な変動や調整がありますので、必ず <#1131285823757758475> をご確認ください！`)
     })
   } 
