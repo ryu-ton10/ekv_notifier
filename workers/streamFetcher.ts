32322c63a0886ee93b1dbd0f6dc85bb368665b80
ryu-ton10 <kanontonto1010@gmail.com>
2026-02-16 07:59:45 +0900

    revise a process of fetching stream url

--- workers/streamFetcher.ts
+++ workers/streamFetcher.ts
@@ -1,5 +1,4 @@
-import { fetchGameMaster } from "./memberFetcher.ts"
-import { fetchRowsFromSheet } from "./spreadsheetWorker.ts"
+import { fetchGameMaster, fetchMember } from "./memberFetcher.ts"
 import 'dotenv/config'
 
 export type VideoUrl = {
@@ -7,27 +6,44 @@ export type VideoUrl = {
   url: string
 }
 
+/**
+ * fetchStreams
+ * 参加メンバーの配信枠を YouTube Data API を利用して取得する
+ *
+ * @param members string[] 参加メンバーの ID 一覧
+ * @return VideoUrl[] 配信枠一覧
+ */
 export const fetchStreams = async (members: string[]): Promise<VideoUrl[]> => {
   const result: VideoUrl[] = []
-  const memberMasterSheetId = process.env.MEMBER_MASTER_WORKSHEET_ID ?? ''
 
   // 決め打ちで GM の配信枠を取得する
   const gm = await fetchGameMaster();
   result.push(await callApi(gm.name, gm.channelId, 'upcoming'))
 
   // 当日参加するメンバーの配信枠を取得する
-  const memberRows = await fetchRowsFromSheet(Number(memberMasterSheetId));
-  for (const r of memberRows) {
-    for (const m of members) {
-      if (r.get('discordId') === m) {
-        result.push(await callApi(r.get('name'), r.get('channelId'), 'upcoming'))
-      }
+  for (const m of members) {
+    const member = await fetchMember(m);
+
+    if (member.channelId === '') {
+      continue;
     }
+
+    const videoUrl = await callApi(member.name, member.channelId, 'upcoming')
+    result.push(videoUrl)
   }
 
   return result
 }
 
+/**
+ * callApi
+ * YouTube Data API を呼び出して配信枠を取得する
+ *
+ * @param name string メンバー名
+ * @param channelId string チャンネル ID
+ * @param eventType string イベントタイプ（upcoming など）
+ * @return VideoUrl 配信枠情報
+ */
 export const callApi = async (name: string, channelId: string, eventType: string): Promise<VideoUrl> => {
   const url = `https://content-youtube.googleapis.com/youtube/v3/search?q=EKV&channelId=${channelId}&maxResults=1&part=snippet&type=video&eventType=${eventType}&key=${process.env.YOUTUBE_API_KEY}`
 
@@ -49,7 +65,7 @@ export const callApi = async (name: string, channelId: string, eventType: string
       url: `https://www.youtube.com/watch?v=${videoId}`
     }
   } catch(error) {
-    console.log(error)
+    console.log("Error fetching stream:", error)
     return { name: '', url: '' }
   }
 }
